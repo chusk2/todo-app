3d815a2ea1962948ca7732aebb801be94dea78cf
Daniel <294.daniel@gmail.com>
2025-12-07 13:38:45 +0100

    Add remove feature

--- read.py
+++ read.py
@@ -1,5 +1,6 @@
 import sqlite3
 import streamlit as st
+from streamlit import column_config
 import pandas as pd
 
 def read_tasks():
@@ -14,7 +15,13 @@ def read_tasks():
         # Convert date-like strings to actual datetime objects for the data editor
         tasks_df['creation_date'] = pd.to_datetime(tasks_df['creation_date'])
         tasks_df['completed_date'] = pd.to_datetime(tasks_df['completed_date'], errors='coerce') # 'coerce' handles NULLs gracefully
+
+        # fillna de completed_date con no completada
+        tasks_df.completed_date.fillna('Aún no completada', inplace=True)
         
+        # hide task_id column
+        tasks_df.drop('task_id', axis=1, inplace=True)
+
         return tasks_df
     except Exception as e:
         print(e)
@@ -27,25 +34,35 @@ st.header('Your List of Tasks')
 
 tasks_df = read_tasks()
 
+cfg = dict.fromkeys(tasks_df.columns)
+cfg = {i: column_config.Column(width=None) for i in cfg.keys()}
+
 if not tasks_df.empty:
-    edited_df = st.data_editor(
-        tasks_df,
-        column_config={
-            "task_id": None, # Hide the task_id column
-            "task": st.column_config.TextColumn("Task Description", width="large"),
-            "creation_date": st.column_config.DatetimeColumn(
-                "Created On",
-                format="YYYY-MM-DD HH:mm:ss",
-            ),
-            "completed": st.column_config.CheckboxColumn("Status", default=False),
-            "completed_date": st.column_config.DatetimeColumn(
-                "Completed On",
-                format="YYYY-MM-DD HH:mm:ss",
-            ),
-        },
-        hide_index=True,
-        use_container_width=True
-    )
-    # Here you would add logic to detect changes and update the database
+    # Mostrar la tabla
+    st.dataframe(tasks_df,
+      column_config=cfg,
+      use_container_width=True,  # Auto-ajuste al contenedor (default)
+      hide_index=True
+  )
+    #st.write(tasks_df)
+    # edited_df = st.data_editor(
+    #     tasks_df,
+    #     column_config={
+    #         "task_id": None, # Hide the task_id column
+    #         "task": st.column_config.TextColumn("Task Description", width="large"),
+    #         "creation_date": st.column_config.DatetimeColumn(
+    #             "Created On",
+    #             format="YYYY-MM-DD HH:mm:ss",
+    #         ),
+    #         "completed": st.column_config.CheckboxColumn("Status", default=False),
+    #         "completed_date": st.column_config.DatetimeColumn(
+    #             "Completed On",
+    #             format="YYYY-MM-DD HH:mm:ss",
+    #         ),
+    #     },
+    #     hide_index=True,
+    #     use_container_width=True
+    # )
+    # # Here you would add logic to detect changes and update the database
 else:
     st.info("Your task list is empty. Use the 'Add a task' page to get started!")
